a92a2e0dec1f83a4e9920abb57e10f04f9395a9c
Luis Cielak <luis.cielak@gmail.com>
2018-06-01 14:38:07 -0400

    Update heading text

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/header'
 import Heading from './components/heading'
 
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Header text='Standup Analytica' />
-        <Heading text='Another Heading' />
-        <Button text='' />
+        <Heading text='Whatcha did, Whatcha doin, Whatcha gonna do' />
+
       </View>
     );
   }
