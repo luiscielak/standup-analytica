b5137ce739badd16c8bfb0a94b81e8fe3ddc9298
Luis Cielak <luis.cielak@gmail.com>
2018-06-01 14:51:15 -0400

    Add button component

---
+++ components/button.js
@@ -0,0 +1,14 @@
+import { Button } from 'react-native';
+
+export default class Button extends React.Component {
+  render() {
+    return (
+      <View>
+        <Button
+          title="Stand Up!"
+          color="#FF9500"
+        />
+      </View>
+    );
+  }
+}
