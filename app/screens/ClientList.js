254dbddf910d7dafe1b0f7666c908acb68aa5258
Luis Cielak <luis.cielak@gmail.com>
2018-07-16 01:27:14 -0400

    Fix error where components dir was not found

--- app/screens/ClientList.js
+++ app/screens/ClientList.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { View, Text } from 'react-native'
 
-import ScreenContainer from '../Components/ScreenContainer'
+import ScreenContainer from '../components/ScreenContainer'
 
 export default class ClientList extends PureComponent {
   render () {
